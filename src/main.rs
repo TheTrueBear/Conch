9ab32a4cf96dad5a81c60d93b48d2af1346279f3
Your Name <thisisatest@gmail.com>
2022-12-09 20:48:10 -0800

    Working on lexer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+mod token;
 
 fn main() {
     println!("Hello, world!");
--- src/token.rs
+++ src/token.rs
@@ -3,11 +3,18 @@
 /* Token class  */
 pub struct Token {
     pub type_ : String,
-    pub data : (String, i8),
+    pub data : i8,
 }
 
 /* Token initializer  */
 pub fn new(type_ : String, data: Option<i8>) -> Token {
     let mut token = Token(type_, data.unwrap_or(0));
     return token;
+}
+
+/* Token functions */
+pub impl Token {
+    fn repr(&self) -> String {
+
+    }
 }
